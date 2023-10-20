419df6aaccec831462e6193c6af4d7a65264bf26
CarlosSilva0912 <148591268+CarlosSilva0912@users.noreply.github.com>
2023-10-20 18:36:38 -0300

    OlaMundo e Teclado

---
+++ aula01/src/aula01/Teclado.java
@@ -0,0 +1,14 @@
+package aula01;
+import java.util.Scanner;
+
+public class Teclado {
+	public static void main(String[] argumentos) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite seu nome: ");
+		String nome = scanner.nextLine();
+		System.out.print("Digite sua idade: ");
+		int idade = scanner.nextInt();
+		System.out.println("Nome: " + nome);
+		System.out.println("Idade: " + idade);
+	}
+}
